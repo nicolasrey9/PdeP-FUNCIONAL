b1d893b5313b0811884d1a6e0e098f48a21d982a
Nicolas Rey <nicolasreypc@gmail.com>
2024-05-23 17:16:41 -0300

    hasta donde hice

---
+++ carreras_funcional_2021.hs
@@ -0,0 +1,150 @@
+-- https://docs.google.com/document/d/1g2Gc81R62_xAIiGF0H663ypAz1vxJybr5LDo1sj9tAU/edit#heading=h.ielqgky5ojzp
+--------------------------------------------------------------
+------------------------ PUNTO 1 -----------------------------
+--------------------------------------------------------------
+import Data.List
+data Auto = Auto{
+    color :: String,
+    velocidad :: Int,
+    distancia :: Int
+} deriving(Show, Eq)
+
+type Carrera = [Auto]
+
+estaCerca :: Auto -> Auto -> Bool
+estaCerca auto1 auto2 = coloresDistintos auto1 auto2 && distanciaMenorA10 auto1 auto2
+
+vaTranquilo :: Auto -> Carrera -> Bool
+vaTranquilo auto carrera = vaPrimero auto carrera && noTieneANadieCerca auto carrera
+
+puesto :: Auto -> Carrera -> Int
+puesto auto = (+1) . length . filter (flip leGana auto)
+
+--
+coloresDistintos :: Auto -> Auto -> Bool
+coloresDistintos auto1 = (/= color auto1) . color
+
+distanciaMenorA10 :: Auto -> Auto -> Bool
+distanciaMenorA10 auto1 = (10>) . abs . (distancia auto1 -) . distancia
+
+noTieneANadieCerca :: Auto -> Carrera -> Bool
+noTieneANadieCerca auto = not . any (estaCerca auto)
+
+vaPrimero :: Auto -> Carrera -> Bool
+vaPrimero auto = all (leGana auto)
+
+leGana :: Auto -> Auto -> Bool
+leGana ganador perdedor = distancia ganador > distancia perdedor
+--------------------------------------------------------------
+------------------------ PUNTO 2 -----------------------------
+--------------------------------------------------------------
+autoCorraDurante :: Int -> Auto -> Auto
+autoCorraDurante tiempo auto = auto{distancia = distancia auto + tiempo * velocidad auto}
+
+type Modificador = Int -> Int
+
+alterarVelocidad :: Modificador -> Auto -> Auto
+alterarVelocidad modificador auto = auto{velocidad = modificador (velocidad auto)}
+
+bajarVelocidad :: Int -> Modificador
+bajarVelocidad velocidadABajar velocidadActual
+    | velocidadABajar >= velocidadActual = 0
+    | otherwise                          = velocidadActual - velocidadABajar
+--------------------------------------------------------------
+------------------------ PUNTO 3 -----------------------------
+--------------------------------------------------------------
+afectarALosQueCumplen :: (Auto -> Bool) -> (Auto -> Auto) -> Carrera -> Carrera
+afectarALosQueCumplen criterio efecto carrera = (map efecto . filter criterio) carrera ++ filter (not.criterio) carrera
+
+type PowerUp = Auto -> Carrera -> Carrera
+
+terremoto :: PowerUp
+terremoto auto = afectarALosQueCumplen (estaCerca auto) (alterarVelocidad (bajarVelocidad 50))
+
+miguelitos :: Int -> PowerUp
+miguelitos velABajar auto = afectarALosQueCumplen (leGana auto) (alterarVelocidad (bajarVelocidad velABajar))
+
+jetPack :: Int -> PowerUp
+jetPack tiempoDuracion auto = afectarALosQueCumplen (== auto) (usarJetPack tiempoDuracion)
+
+
+--
+usarJetPack :: Int -> Auto -> Auto
+usarJetPack duracion = alterarVelocidad reducirAMitadVel . autoCorraDurante duracion . alterarVelocidad duplicarVel
+
+duplicarVel :: Modificador
+duplicarVel = (*2)
+
+reducirAMitadVel :: Modificador
+reducirAMitadVel = (`div`2) 
+--------------------------------------------------------------
+------------------------ PUNTO 4 -----------------------------
+--------------------------------------------------------------
+type Evento = Carrera -> Carrera
+type TablaDePosiciones = [(Int, String)]
+
+simularCarrera :: Carrera -> [Evento] -> TablaDePosiciones
+simularCarrera carrera = armarTabla . llegarAlEstadoFinalDeCarrera carrera
+
+llegarAlEstadoFinalDeCarrera :: Carrera -> [Evento] -> Carrera
+llegarAlEstadoFinalDeCarrera = foldl (flip($))
+
+armarTabla :: Carrera -> TablaDePosiciones
+armarTabla = transformarATabla . ordenarCompetidores
+
+-- resolucion dudosa
+ordenarCompetidores :: Carrera -> [Auto]
+ordenarCompetidores carrera = emparejarListas (reverse . sort . map distancia $ carrera) carrera
+
+emparejarListas :: [Int] -> Carrera -> [Auto]
+emparejarListas _  []  = []
+emparejarListas _ [x]  = [x]
+emparejarListas (x:xs) autosDesorden = emparejarElem x autosDesorden ++ emparejarListas xs autosDesorden
+
+emparejarElem :: Int -> Carrera -> [Auto]
+emparejarElem distanciaAEmparejar = take 1 . filter ((==distanciaAEmparejar) . distancia)
+
+transformarATabla :: [Auto] -> [(Int, String)]
+transformarATabla autosOrdenados = zip [1 .. length autosOrdenados] (map color autosOrdenados)
+--
+
+
+
+correnTodos :: Int -> Evento
+correnTodos tiempo carrera = map (autoCorraDurante tiempo) carrera
+
+-- type PowerUp = Auto -> Carrera -> Carrera
+-- type Evento = Carrera -> Carrera
+usaPowerUp :: PowerUp -> String -> Evento
+usaPowerUp powerUp colorX carrera = 
+
+--------------------------------------------------------------
+------------------------ PUNTO 1 -----------------------------
+--------------------------------------------------------------
+
+--------------------------------------------------------------
+------------------------ PUNTO 1 -----------------------------
+--------------------------------------------------------------
+
+--------------------------------------------------------------
+------------------------ PUNTO 1 -----------------------------
+--------------------------------------------------------------
+
+--------------------------------------------------------------
+------------------------ PUNTO 1 -----------------------------
+--------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
