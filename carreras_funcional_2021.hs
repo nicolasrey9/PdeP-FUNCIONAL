81f03017f94649e948261c4e1942be3100b061ae
Nicolas Rey <nicolasreypc@gmail.com>
2024-05-23 20:20:15 -0300

    .

--- carreras_funcional_2021.hs
+++ carreras_funcional_2021.hs
@@ -2,6 +2,8 @@
 --------------------------------------------------------------
 ------------------------ PUNTO 1 -----------------------------
 --------------------------------------------------------------
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use section" #-}
 import Data.List
 data Auto = Auto{
     color :: String,
@@ -12,26 +14,20 @@ data Auto = Auto{
 type Carrera = [Auto]
 
 estaCerca :: Auto -> Auto -> Bool
-estaCerca auto1 auto2 = coloresDistintos auto1 auto2 && distanciaMenorA10 auto1 auto2
+estaCerca auto1 auto2 = auto1 /= auto2 && distanciaMenorA10 auto1 auto2
 
 vaTranquilo :: Auto -> Carrera -> Bool
-vaTranquilo auto carrera = vaPrimero auto carrera && noTieneANadieCerca auto carrera
+vaTranquilo auto = all (leGanaYNoLoTieneCerca auto)
+
+leGanaYNoLoTieneCerca :: Auto -> Auto -> Bool
+leGanaYNoLoTieneCerca auto1 auto2 = leGana auto1 auto2 && not (estaCerca auto1 auto2)
 
 puesto :: Auto -> Carrera -> Int
 puesto auto = (+1) . length . filter (flip leGana auto)
 
 --
-coloresDistintos :: Auto -> Auto -> Bool
-coloresDistintos auto1 = (/= color auto1) . color
-
 distanciaMenorA10 :: Auto -> Auto -> Bool
 distanciaMenorA10 auto1 = (10>) . abs . (distancia auto1 -) . distancia
-
-noTieneANadieCerca :: Auto -> Carrera -> Bool
-noTieneANadieCerca auto = not . any (estaCerca auto)
-
-vaPrimero :: Auto -> Carrera -> Bool
-vaPrimero auto = all (leGana auto)
 
 leGana :: Auto -> Auto -> Bool
 leGana ganador perdedor = distancia ganador > distancia perdedor
@@ -44,12 +40,11 @@ autoCorraDurante tiempo auto = auto{distancia = distancia auto + tiempo * veloci
 type Modificador = Int -> Int
 
 alterarVelocidad :: Modificador -> Auto -> Auto
-alterarVelocidad modificador auto = auto{velocidad = modificador (velocidad auto)}
+alterarVelocidad modificador auto = auto{velocidad = max 0 (modificador . velocidad $ auto)}
 
-bajarVelocidad :: Int -> Modificador
-bajarVelocidad velocidadABajar velocidadActual
-    | velocidadABajar >= velocidadActual = 0
-    | otherwise                          = velocidadActual - velocidadABajar
+bajarVelocidad :: Int -> Auto -> Auto
+bajarVelocidad cantidadABajar = alterarVelocidad (cantidadABajar - )
+
 --------------------------------------------------------------
 ------------------------ PUNTO 3 -----------------------------
 --------------------------------------------------------------
@@ -59,29 +54,28 @@ afectarALosQueCumplen criterio efecto carrera = (map efecto . filter criterio) c
 type PowerUp = Auto -> Carrera -> Carrera
 
 terremoto :: PowerUp
-terremoto auto = afectarALosQueCumplen (estaCerca auto) (alterarVelocidad (bajarVelocidad 50))
+terremoto auto = alterarVelocidadDeAutosSegun (estaCerca auto) 50
 
 miguelitos :: Int -> PowerUp
-miguelitos velABajar auto = afectarALosQueCumplen (leGana auto) (alterarVelocidad (bajarVelocidad velABajar))
+miguelitos velABajar auto = alterarVelocidadDeAutosSegun (leGana auto) velABajar
 
 jetPack :: Int -> PowerUp
 jetPack tiempoDuracion auto = afectarALosQueCumplen (== auto) (usarJetPack tiempoDuracion)
 
-
 --
+alterarVelocidadDeAutosSegun :: (Auto -> Bool) -> Int -> Carrera -> Carrera
+alterarVelocidadDeAutosSegun criterio velocidad = afectarALosQueCumplen criterio (bajarVelocidad velocidad)
+
 usarJetPack :: Int -> Auto -> Auto
-usarJetPack duracion = alterarVelocidad reducirAMitadVel . autoCorraDurante duracion . alterarVelocidad duplicarVel
+usarJetPack duracion = alterarVelocidad (`div`2) . autoCorraDurante duracion . alterarVelocidad (*2)
 
-duplicarVel :: Modificador
-duplicarVel = (*2)
 
-reducirAMitadVel :: Modificador
-reducirAMitadVel = (`div`2)
 --------------------------------------------------------------
 ------------------------ PUNTO 4 -----------------------------
 --------------------------------------------------------------
 type Evento = Carrera -> Carrera
 type TablaDePosiciones = [(Int, String)]
+{-
 
 simularCarrera :: Carrera -> [Evento] -> TablaDePosiciones
 simularCarrera carrera = armarTabla . llegarAlEstadoFinalDeCarrera carrera
@@ -89,69 +83,23 @@ simularCarrera carrera = armarTabla . llegarAlEstadoFinalDeCarrera carrera
 llegarAlEstadoFinalDeCarrera :: Carrera -> [Evento] -> Carrera
 llegarAlEstadoFinalDeCarrera = foldl (flip ($))
 
-armarTabla :: Carrera -> TablaDePosiciones
-armarTabla = transformarATabla . ordenarCompetidores
-
--- resolucion dudosa
-ordenarCompetidores :: Carrera -> [Auto]
-ordenarCompetidores carrera = emparejarListas (reverse . sort . map distancia $ carrera) carrera
-
-emparejarListas :: [Int] -> Carrera -> [Auto]
-emparejarListas _  []  = []
-emparejarListas _ [x]  = [x]
-emparejarListas (x:xs) autosDesorden = emparejarElem x autosDesorden ++ emparejarListas xs autosDesorden
-
-emparejarElem :: Int -> Carrera -> [Auto]
-emparejarElem distanciaAEmparejar = take 1 . filter ((==distanciaAEmparejar) . distancia)
-
-transformarATabla :: [Auto] -> [(Int, String)]
-transformarATabla autosOrdenados = zip [1 .. length autosOrdenados] (map color autosOrdenados)
---
+armarTabla :: Carrera -> TablaDePosiciones -}
+-----------------------------------------------------------
 
 
 
 correnTodos :: Int -> Evento
 correnTodos tiempo = map (autoCorraDurante tiempo)
 
--- type PowerUp = Auto -> Carrera -> Carrera
--- type Evento = Carrera -> Carrera
--- afectarALosQueCumplen :: (Auto -> Bool) -> (Auto -> Auto) -> Carrera -> Carrera
--- afectarALosQueCumplen criterio efecto carrera = (map efecto . filter criterio) carrera ++ filter (not.criterio) carrera
 
--- Desarrollar las siguientes funciones de modo que puedan usarse para generar los eventos que se dan en una carrera:
--- usaPowerUp que a partir de un power up y del color del auto que gatilló el poder en cuestión, 
--- encuentre el auto correspondiente dentro del estado actual de la carrera para usarlo y 
--- produzca los efectos esperados para ese power up.
 usarPowerUp :: PowerUp -> String -> Evento
 usarPowerUp powerUp colorX carrera = powerUp (encontrarAutoColor colorX carrera) carrera
 encontrarAutoColor :: String -> Carrera -> Auto
 encontrarAutoColor colorx = head . filter ((==colorx) . color)
 
 --------------------------------------------------------------
------------------------- PUNTO 1 -----------------------------
+------------------------ PUNTO 5 -----------------------------
 --------------------------------------------------------------
-
---------------------------------------------------------------
------------------------- PUNTO 1 -----------------------------
---------------------------------------------------------------
-
---------------------------------------------------------------
------------------------- PUNTO 1 -----------------------------
---------------------------------------------------------------
-
---------------------------------------------------------------
------------------------- PUNTO 1 -----------------------------
---------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
 
 
 
